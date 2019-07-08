57a6d0e576e6ec4d3066673af284e9c0caf90a99
Spencer Owen <owenspencer@gmail.com>
2019-07-07 23:46:31 -0600

    Kind of works

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -2,7 +2,7 @@ module StaticPagesHelper
   def fourth
     #TODO: This assumes UTC so will be wrong for a lot of people.
     d = Date.current.in_time_zone
-    @fourth = "NO"
-    @fourth = "Heck YA!" if d.month == 7 && d.day == 8
+    @fourth = false
+    @fourth = true if d.month == 7 && d.day == 9
   end
 end
