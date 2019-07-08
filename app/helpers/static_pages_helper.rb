b613ed6a85b7678ce734a42e7dcc74f9ef4924c9
Spencer Owen <owenspencer@gmail.com>
2019-07-07 23:50:11 -0600

    reverse

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -3,6 +3,6 @@ module StaticPagesHelper
     #TODO: This assumes UTC so will be wrong for a lot of people.
     d = Date.current.in_time_zone
     @fourth = false
-    @fourth = true if d.month == 7 && d.day == 9
+    @fourth = true if d.month == 7 && d.day == 4
   end
 end
